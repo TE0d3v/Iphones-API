1207975922ba96a1682020857641f8060d692379
Gabriel Angelo <gabrielangelolira@gmail.com>
2025-10-07 21:15:44 -0300

    criado a rota de gerar um novo iphone

---
+++ src/controllers/iphone.controller.js
@@ -0,0 +1,15 @@
+const Iphone = require('../models/Iphone');
+
+async function createIphone(req, res) {
+    try {
+        const { model, color, storage, price } = req.body;
+        const newIphone = await Iphone.create({ model, color, storage, price });
+        res.status(201).json(newIphone);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+module.exports = {
+    createIphone,
+};
